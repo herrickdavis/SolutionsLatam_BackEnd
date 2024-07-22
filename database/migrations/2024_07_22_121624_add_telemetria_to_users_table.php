eb28b6059c2e79556bad05b6bda8ba790aa4f32c
Jhony Diaz <urbanodiazq@gmail.com>
2024-07-22 15:52:32 -0500

    fix db

--- database/migrations/2024_07_22_121624_add_telemetria_to_users_table.php
+++ database/migrations/2024_07_22_121624_add_telemetria_to_users_table.php
@@ -14,7 +14,7 @@ class AddTelemetriaToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('telemetria', ['S', 'N'])->nullable()->after('data_campo');;
+            $table->enum('telemetria', ['S', 'N'])->default('N')->after('data_campo');;
         });
     }
 
